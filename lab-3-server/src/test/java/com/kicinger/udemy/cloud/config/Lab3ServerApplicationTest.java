2083b8eb255abe3d854a1e7f58dffce5eb99ac16
krzysztofkicinger <krzysztofkicinger.public@gmail.com>
2017-10-26 09:13:06 +0200

    [Lab3] lab-3-client

--- lab-3-server/src/test/java/com/kicinger/udemy/cloud/config/Lab3ServerApplicationTest.java
+++ lab-3-server/src/test/java/com/kicinger/udemy/cloud/config/Lab3ServerApplicationTest.java
@@ -1,11 +1,16 @@
 package com.kicinger.udemy.cloud.config;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,9 +21,10 @@ public class Lab3ServerApplicationTest {
 
     @Test
     public void whenClientWantsToGetConfigShouldBeSendProperties() {
-        String body = restTemplate.getForEntity("/lab-3-client/default", String.class)
-                .getBody();
-        System.out.println(body);
+        ResponseEntity<String> response = restTemplate.getForEntity("/lab-3-client/default", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat()
+//        System.out.println(body);
     }
 
 }
